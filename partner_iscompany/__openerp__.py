4100bb45dcf94bd8059b522d967ad542cffb4065
Alexsandro Haag <alex@hgsoft.com.br>
2016-11-03 23:31:02 -0200

    Iniciados módulos partner_iscompany e partner_serasa

---
+++ partner_iscompany/__openerp__.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+{
+    'name': 'partner_iscompany',
+    'category': 'Sale',
+    'summary': 'Muda o padrão da criação de parceiros, onde "is company" é definido como padrão',
+    'version': '1.0',
+    'description': 'Muda o padrão da criação de parceiros, onde "is company" é definido como padrão',
+    'author': 'HGSOFT - Alexsandro Haag',
+    'website': 'http://www.hgsoft.com.br',
+    'depends': ['base','sale'],
+    'data': [
+        'data/base_data.xml',
+    ],
+    'installable': True,
+}
