e52474afbf402907877383babd9e801e8e69a5ec
Alexsandro Haag <alex@hgsoft.com.br>
2016-11-14 01:21:28 -0200

    Novo módulo para preencher o preço de revenda sugerido ao cliente

---
+++ product_sale_price_reseller/__openerp__.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+{
+    'name': 'product_sale_price_reseller',
+    'category': 'Sale',
+    'summary': 'Acrescenta um campo para informar o preço sugerido de revenda, para exibir no Website',
+    'version': '1.0',
+    'description': 'Acrescenta um campo para informar o preço sugerido de revenda, para exibir no Website',
+    'author': 'HGSOFT - Alexsandro Haag',
+    'website': 'http://www.hgsoft.com.br',
+    'depends': ['base','sale'],
+    'installable': True,
+}
