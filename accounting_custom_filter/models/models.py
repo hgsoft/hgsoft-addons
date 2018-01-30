cc549009fd2aa39970ad83fc4302ade48a1af57b
Ewerton Rover <ewerton@hgsoft.com.br>
2018-01-30 16:20:23 -0200

    View com as informações necessárias, finalizada. Falta ajustar os filtros e group by.

--- accounting_custom_filter/models/models.py
+++ accounting_custom_filter/models/models.py
@@ -4,14 +4,8 @@ from odoo import models, fields, api
 
 class customAccountInvoice(models.Model):
     
-    _inherit = 'account.invoice'
-#     _name = 'accounting_custom_filter.accounting_custom_filter'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    _inherit = 'account.invoice.line'
+        
+    #cfop_code = fields.Char('CFOP', related='cfop_id.code', readonly=True)
+    
+    fiscal_position = fields.Char('Fiscal Position', related='invoice_id.fiscal_position_id.name', readonly=True) 
