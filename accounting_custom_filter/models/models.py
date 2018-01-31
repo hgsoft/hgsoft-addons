0715bb7daa28a7793038edde353e3ed0ccd57068
Ewerton Rover <ewerton@hgsoft.com.br>
2018-01-31 12:00:03 -0200

    Adicionado campo de controle para computar valores a serem agrupados na view[Finalizados]. Adicionar agrupamentos restantes.

--- accounting_custom_filter/models/models.py
+++ accounting_custom_filter/models/models.py
@@ -6,6 +6,38 @@ class customAccountInvoice(models.Model):
     
     _inherit = 'account.invoice.line'
         
-    #cfop_code = fields.Char('CFOP', related='cfop_id.code', readonly=True)
+    #code = fields.Char('CFOP', default='CFOP')
     
-    fiscal_position = fields.Char('Fiscal Position', related='invoice_id.fiscal_position_id.name', readonly=True) 
+    #fiscal_position = fields.Char('Fiscal Position', related='invoice_id.fiscal_position_id.name', readonly=True) 
+    
+    #####
+    
+    code = fields.Char('CFOP', compute='_compute_group_by_vars', store=True)
+    
+    fiscal_position = fields.Char('Fiscal Position', compute='_compute_group_by_vars', store=True) 
+    
+    @api.one
+    def _compute_group_by_vars(self):
+        print ("##### _compute_group_by_vars [START] #####")
+        
+        self.code = self.cfop_id.code
+        
+        self.fiscal_position = self.invoice_id.fiscal_position_id.name
+        
+        print ("##### _compute_group_by_vars [END] #####")
+
+
+    """
+    #@api.model
+    @api.depends('code')
+    def test(self):
+        print ("##### depends [START] #####")
+        
+        print ("##### depends [END] #####")
+        
+    @api.onchange('code')
+    def test(self):
+        print ("##### onchange [START] #####")
+        
+        print ("##### onchange [END] #####")
+    """
