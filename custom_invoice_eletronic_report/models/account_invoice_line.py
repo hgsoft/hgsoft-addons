02088da44741344e94ad5a86c6720563fbab011a
Ewerton Rover <ewerton@hgsoft.com.br>
2018-10-25 10:11:43 -0200

    [TST] - Teste com versão final.

--- custom_invoice_eletronic_report/models/account_invoice_line.py
+++ custom_invoice_eletronic_report/models/account_invoice_line.py
@@ -9,12 +9,22 @@ class customInvoiceEletronic(models.Model):
     cfop_code = fields.Char('cfop_code')    
 
     @api.multi
-    def create(self, vals):
-    
-        print ("###########################")
-        print ("###### create [START] #####")
-        print ("###########################")
+    def create(self, vals):    
+        invoice_eletronic_list = self.env['invoice.eletronic'].search([('cfop_code','=', None)])
         
+        for invoice_eletronic in invoice_eletronic_list:
+            icms_rules_update = invoice_eletronic.fiscal_position_id.icms_tax_rule_ids
+            vals_update = {}
+            if len(icms_rules_update) > 1:
+                rules_cfop_update = []
+                for rule_update in icms_rules_update:
+                    rules_cfop_update.append(rule_update.cfop_id.code)
+                vals_update['cfop_code'] = str(rules_cfop_update).replace('[', '').replace(']', '').replace('\'', '')
+            else:
+                vals_update['cfop_code'] = icms_rules_update.cfop_id.code
+                                    
+            super(customInvoiceEletronic, invoice_eletronic).write(vals_update)
+            
         fiscal_position = self.env['account.fiscal.position'].search([('id','=', vals['fiscal_position_id'])])                
                 
         icms_rules = fiscal_position.icms_tax_rule_ids
@@ -28,9 +38,5 @@ class customInvoiceEletronic(models.Model):
             vals['cfop_code'] = icms_rules.cfop_id.code
                                 
         new_invoice_eletronic = super(customInvoiceEletronic, self).create(vals)                
-        
-        print ("###########################")
-        print ("####### create [END] ######")    
-        print ("###########################")
         
         return new_invoice_eletronic
