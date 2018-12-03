878cdbd5ced9cecfb78d6f215510519e70283bc8
Ewerton Rover <ewerton@hgsoft.com.br>
2018-12-03 10:28:52 -0200

    [ADD - Adicionados módulo em migração v10 -> v11.]

--- custom_account_payment_v11/models/account_payment.py
+++ custom_account_payment_v11/models/account_payment.py
@@ -10,6 +10,10 @@ class customAccountPayment(models.Model):
     def default_get(self, fields):
         rec = super(customAccountPayment, self).default_get(fields)
         invoice_defaults = self.resolve_2many_commands('invoice_ids', rec.get('invoice_ids'))
+        #
+        print(rec)
+        print(invoice_defaults)
+        #
         if invoice_defaults and len(invoice_defaults) == 1:
             invoice = invoice_defaults[0]
             rec['communication'] = invoice['origin'] or invoice['reference'] or invoice['name'] or invoice['number']                                
