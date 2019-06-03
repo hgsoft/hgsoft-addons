fa0dee4a8cc15f7bcf4667b007c3c1b83a990c9d
Ewerton Rover <ewerton@hgsoft.com.br>
2019-06-03 13:59:06 -0300

    [ADD] - Adicionado módulo para ajuste de campos da NFe.

---
+++ custom_invoice_eletronic_tax_v11/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#                                                                             #
+# Copyright (C) 2019 HGSOFT - www.hgsoft.com.br                               #
+#                                                                             #
+#This program is free software: you can redistribute it and/or modify         #
+#it under the terms of the GNU Affero General Public License as published by  #
+#the Free Software Foundation, either version 3 of the License, or            #
+#(at your option) any later version.                                          #
+#                                                                             #
+#This program is distributed in the hope that it will be useful,              #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+#GNU General Public License for more details.                                 #
+#                                                                             #
+#You should have received a copy of the GNU General Public License            #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+###############################################################################
+
+{
+    'name': "Custom Invoice Eletronic Tax",
+
+    'summary': """Invoice Eletronic fax fix.""",
+
+    'description': """Invoice Eletronic tax fix.""",
+
+    'author': "HGSoft - Soluções Criativas e Inteligentes",
+    
+    'website': "http://www.hgsoft.com.br/",
+
+    'category': 'Invoice Eletronic',
+    
+    'version': '11.0.0',
+
+    'depends': ['br_nfe'],
+    
+}
---
+++ custom_invoice_eletronic_tax_v11/models/invoice_eletronic.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+import base64
+import re
+
+try:
+    from pytrustnfe.nfe import autorizar_nfe
+    from pytrustnfe.nfe import xml_autorizar_nfe
+    from pytrustnfe.nfe import retorno_autorizar_nfe
+    from pytrustnfe.nfe import recepcao_evento_cancelamento
+    from pytrustnfe.nfe import consultar_protocolo_nfe
+    from pytrustnfe.certificado import Certificado
+    from pytrustnfe.utils import ChaveNFe, gerar_chave, gerar_nfeproc, \
+        gerar_nfeproc_cancel
+    from pytrustnfe.nfe.danfe import danfe
+    from pytrustnfe.xml.validate import valida_nfe
+    from pytrustnfe.urls import url_qrcode, url_qrcode_exibicao
+except ImportError:
+    _logger.error('Cannot import pytrustnfe', exc_info=True)
+
+class customInvoiceEletronicTax(models.Model):
+    _inherit = 'invoice.eletronic'
+    
+    @api.multi
+    def action_post_validate(self):
+        super(customInvoiceEletronicTax, self).action_post_validate()
+        if self.model not in ('55', '65'):
+            return
+        chave_dict = {
+            'cnpj': re.sub('[^0-9]', '', self.company_id.cnpj_cpf),
+            'estado': self.company_id.state_id.ibge_code,
+            'emissao': self.data_emissao[2:4] + self.data_emissao[5:7],
+            'modelo': self.model,
+            'numero': self.numero,
+            'serie': self.serie.code.zfill(3),
+            'tipo': int(self.tipo_emissao),
+            'codigo': "%08d" % self.numero_controle
+        }
+        self.chave_nfe = gerar_chave(ChaveNFe(**chave_dict))
+
+        cert = self.company_id.with_context(
+            {'bin_size': False}).nfe_a1_file
+        cert_pfx = base64.decodestring(cert)
+
+        certificado = Certificado(
+            cert_pfx, self.company_id.nfe_a1_password)
+
+        nfe_values = self._prepare_eletronic_invoice_values()
+
+        ###
+        for value in nfe_values['detalhes']:
+            if value['imposto']['ICMS']['CST'] == '500':
+                value['imposto']['ICMS']['pST'] = '0.00'
+                value['imposto']['ICMS']['vBCSTRet'] = '0.00'
+                value['imposto']['ICMS']['vICMSSTRet'] = '0.00'
+                value['imposto']['ICMS']['vICMSSubstituto'] = '0.00'
+        ###
+
+        lote = self._prepare_lote(self.id, nfe_values)
+
+        xml_enviar = xml_autorizar_nfe(certificado, **lote)
+
+        mensagens_erro = valida_nfe(xml_enviar)
+
+        if mensagens_erro:
+            raise UserError(mensagens_erro)
+
+        self.xml_to_send = base64.encodestring(
+            xml_enviar.encode('utf-8'))
+        self.xml_to_send_name = 'nfse-enviar-%s.xml' % self.numero
