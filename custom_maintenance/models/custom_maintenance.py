b2d63cfe5f951dcae95df4cd410c92bf3720f0e0
Ewerton Rover <ewerton@hgsoft.com.br>
2021-01-19 16:28:22 -0300

    [UPD] - Finalizados ajustes para testar na base.

--- custom_maintenance/models/custom_maintenance.py
+++ custom_maintenance/models/custom_maintenance.py
@@ -12,54 +12,24 @@ class CustomMaintenance(models.Model):
     image_128 = fields.Binary('Image 128', related='product_id.image_128')
     
     kanban_state = fields.Selection([
-        ('0', 'máquina desligada, sem energia'),
-        ('1', 'máquina energizada mas parada'),
-        ('2', 'preparação 1'),
-        ('3', 'preparação 2'),
-        ('4', 'preparação 3'),
-        ('5', 'máquina pronta'),
-        ('6', 'máquina sendo operada manualmente'),
-        ('7', 'máquina trabalhando sem repetibilidade'),
-        ('8', 'máquina trabalhando com repetibilidade'),
-        ('9', 'máquina produzindo com repetibilidade'),
-        ('10', 'máquina produzindo com repetibilidade e qualidade')
+        ('0', 'machine off, no power'),
+        ('1', 'machine energized but stopped'),
+        ('2', 'preparation 1'),
+        ('3', 'preparation 2'),
+        ('4', 'preparation 3'),
+        ('5', 'machine ready'),
+        ('6', 'machine being operated manually'),
+        ('7', 'machine working without repeatability'),
+        ('8', 'machine working with repeatability'),
+        ('9', 'machine producing with repeatability'),
+        ('10', 'machine producing with repeatability and quality')
     ], string='Kanban State', copy=False, default='0', required=True)
     
-    '''
-    máquina desligada, sem energia
-    máquina energizada mas parada
-    preparação 1
-    preparação 2
-    preparação 3
-    máquina pronta
-    máquina sendo operada manualmente
-    máquina trabalhando sem repetibilidade
-    máquina trabalhando com repetibilidade
-    máquina produzindo com repetibilidade
-    máquina produzindo com repetibilidade e qualidade
-    '''
-    
-    @api.onchange('color', 'kanban_state')
-    def _compute_color_and_kanban_state(self):
-        pass
-        '''
-        if self.kanban_state == 'done':
-            self.color = 10
-        elif self.kanban_state == 'blocked':
-            self.color = 1
-        else:
-            self.color = 0
-        '''
+    @api.constrains('kanban_state')
+    def _onchange_kanban_state(self):
+        self.color = int(self.kanban_state)
             
     @api.model
     def sync_colors(self):
-        pass
-        '''
         for equipment in self.search([]):
-            if equipment.color == 10:
-                equipment.kanban_state = 'done'
-            elif equipment.color == 1:
-                equipment.kanban_state = 'blocked'
-            else:
-                equipment.kanban_state = 'normal'
-        '''
+            equipment.kanban_state = str(equipment.color)
