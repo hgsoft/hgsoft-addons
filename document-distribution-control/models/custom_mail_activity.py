405654c0112e76557f71ad137db6f9069276b112
Ewerton Rover <ewerton@hgsoft.com.br>
2021-01-26 17:32:42 -0300

    [UPD] - Adicionados comentários e código inicial para a próxima funcionalidade do módulo.

--- document-distribution-control/models/custom_mail_activity.py
+++ document-distribution-control/models/custom_mail_activity.py
@@ -11,6 +11,18 @@ class CustomMailActivity(models.Model):
         
     @api.model
     def create(self, vals):
+        
+        print('=' * 30)
+        print('')
+        print('[CREATE][START]')
+        #print(vals)
+        #print(self.env.__dict__)
+        obj = self.env['document.page'].search([])
+        print(obj)
+        print('[CREATE][START]')
+        print('')
+        print('=' * 30)
+        
         user_ids = [vals['user_id']] + vals['user_ids'][0][2]
         vals['user_ids'] = None
         vals['show_user_ids'] = False
@@ -40,3 +52,16 @@ class CustomMailActivity(models.Model):
             # Edição
             return {'domain': {'user_ids': [('user_ids', '=', None)]}}
 
+
+    '''
+    
+    Usar: default_res_model, default_res_id
+    
+    Valida se default_res_model = document.page
+    
+    Busca self.env... pelo default_res_id
+    
+    Seta os dados que forem necessários (ex.: revisão) na activity com base no objeto retornado
+    
+    {'cr': <odoo.sql_db.Cursor object at 0x7f4c365f4c88>, 'uid': 2, 'context': {'lang': 'pt_BR', 'tz': 'Europe/Brussels', 'uid': 2, 'allowed_company_ids': [1], 'default_res_id': 2, 'default_res_model': 'document.page'}, 'su': False, 'args': (<odoo.sql_db.Cursor object at 0x7f4c365f4c88>, 2, {'lang': 'pt_BR', 'tz': 'Europe/Brussels', 'uid': 2, 'allowed_company_ids': [1], 'default_res_id': 2, 'default_res_model': 'document.page'}, False), 'registry': <odoo.modules.registry.Registry object at 0x7f4c3822c5f8>, 'cache': <odoo.api.Cache object at 0x7f4c3570dac8>, '_protected': <odoo.tools.misc.StackMap object at 0x7f4c3f6c3678>, 'all': <odoo.api.Environments object at 0x7f4c3570de48>}
+    '''
